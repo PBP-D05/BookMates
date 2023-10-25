70bd8006f8bf7c534244b0707a4db654d84b698d
unknown <zazaneryawan@gmail.com>
2023-10-25 14:32:42 +0700

    Database Prototype & Creating Apps

--- MengelolaBuku/models.py
+++ MengelolaBuku/models.py
@@ -2,14 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Guru(models.Model):
-    user = models.OneToOneField(User)
+class Pengguna(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     isGuru = models.BooleanField()
 
 class Buku(models.Model):
     judul = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
+    rating = models.FloatField()
+    num_of_rating = models.IntegerField()
     min_age = models.IntegerField()
     max_age = models.IntegerField()
     desc = models.TextField()
-    user = models.ForeignKey(Guru, on_delete=models.CASCADE)
+    user = models.ForeignKey(Pengguna, on_delete=models.CASCADE)
