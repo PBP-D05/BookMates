a9e52f23bb233698f109023a95293ede8b6d70cf
unknown <zazaneryawan@gmail.com>
2023-10-28 20:10:04 +0700

    Done frontend, need sync with prev page

--- ChallengeLeaderboard/models.py
+++ ChallengeLeaderboard/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 from MengelolaBuku.models import Buku
+from django.contrib.auth.models import User
 
-# Create your models here.
 class Challenge(models.Model):
+    # community = models.ForeignKey(Community) Model Fathur
     name = models.TextField(primary_key=True)
     point = models.IntegerField()
     deadline = models.DateTimeField()
     description = models.TextField()
     books = models.ForeignKey(Buku, on_delete=models.RESTRICT)
     reply = models.TextField() # encoded <CLS> NAME <SEP> REPLY <CLS>
+
+    
--- ChallengeLeaderboard/views.py
+++ ChallengeLeaderboard/views.py
@@ -5,6 +5,9 @@ from django.http import Http404
 from . import models
 
 def leaderboard(request):
+    context = {
+        'komunitas': models.Challenge.objects.get()
+    }
     return render(request, 'leaderboard.html')
 
 def challenge(request, name: str):
