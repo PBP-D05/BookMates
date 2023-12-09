d860df8ab39a66112a851d1ee55a982d106f2515
joydebora <joy.debora@ui.ac.id>
2023-12-09 18:30:31 +0700

    SearchKatalog

--- SearchKatalog/views.py
+++ SearchKatalog/views.py
@@ -12,11 +12,17 @@ from django.db.models import Q
 def search_results(request):
     return render(request, 'search_results.html')
 
+@csrf_exempt
 # View untuk menanggapi permintaan pencarian
 def perform_search(request):
-    if request.method == 'GET':
-        category = request.GET.get('category', '')  # Mendapatkan kategori dari parameter GET
-        keyword = request.GET.get('keyword', '')  # Mendapatkan kata kunci pencarian dari parameter GET
+    if request.method == 'POST':
+        category = request.POST.get('category', '')
+        keyword = request.POST.get('keyword', '')
+        print(category)
+        print(keyword)
+
+        # Print debug information
+        print(f"Received request with category: {category}, keyword: {keyword}")
 
         # Lakukan validasi, pastikan kategori dipilih dan keyword tidak kosong
         if category and keyword:
@@ -43,29 +49,17 @@ def perform_search(request):
             else:
                 # Kategori tidak valid
                 error_message = 'Invalid category.'
-                return render(request, 'search_results.html', {'error_message': error_message})
-            
-            # Ubah hasil pencarian ke dalam format yang sesuai untuk ditampilkan di halaman
-            books = []
-            for book in results:
-                books.append({
-                    'judul': book.judul,
-                    'author': book.author,
-                    'rating': book.rating,
-                    'num_of_rating': book.num_of_rating,
-                    'min_age': book.min_age,
-                    'max_age': book.max_age,
-                    'image_url': book.image_url if book.image_url else '',  # URL gambar jika ada, kosongkan jika tidak
-                    'desc': book.desc,
-                })
-                
-            return JsonResponse({'books': books})
+                return JsonResponse({'error_message': error_message})
+
+            return HttpResponse(serializers.serialize('json', results), content_type="application/json")
+
         else:
             # Jika kategori tidak dipilih atau keyword kosong, tampilkan pesan error
             error_message = 'Please select a category and enter a keyword.'
             return JsonResponse({'error_message': error_message})
+
     else:
-        # Jika permintaan bukan metode GET, tampilkan pesan error
+        # Metode request tidak diizinkan (bukan GET atau POST)
         error_message = 'Invalid request method.'
         return JsonResponse({'error_message': error_message})
 
