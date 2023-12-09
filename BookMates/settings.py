dd6cf7d919f0b61cd03932420fbf6e9bd6f75856
Vv-kK <veronicakylie1@gmail.com>
2023-12-09 13:48:18 +0700

    menyesuaikan variabel login dan membenarkan csrf problem

--- BookMates/settings.py
+++ BookMates/settings.py
@@ -71,6 +71,7 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+CSRF_TRUSTED_ORIGINS = ["https://booksmate-d05-tk.pbp.cs.ui.ac.id/"]
 
 ROOT_URLCONF = 'BookMates.urls'
 
