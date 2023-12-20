6953701e7e7dc3a181ff9088e2e507fa3cc2eb58
Vv-kK <veronicakylie1@gmail.com>
2023-12-20 09:21:11 +0700

    Adding logout

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from authentication.views import leaderboard, login,register
+from authentication.views import leaderboard, login,register, logout
 
 app_name = 'authentication'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('register/', register, name='register'),
     path('leaderboard/', leaderboard, name='leaderboard'),
+    path('logout/', logout, name='logout'),
 ]
